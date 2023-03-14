b6f3ec7ebb57a5802708bc71c21a6c35caae09a1
Leandro Alves da Silva <leandroportnoy@gmail.com>
2023-03-14 07:24:13 -0300

    adding some unit test on data layer

--- data/src/test/java/br/com/las/petshop/data/GetItemsTest.kt
+++ data/src/test/java/br/com/las/petshop/data/GetItemsTest.kt
@@ -1,5 +1,6 @@
 package br.com.las.petshop.data
 
+import br.com.las.petshop.data.data.Item
 import br.com.las.petshop.data.data.ProductList
 import br.com.las.petshop.data.services.RestApi
 import kotlinx.coroutines.runBlocking
@@ -10,12 +11,32 @@ import org.mockito.kotlin.mock
 class GetItemsTest {
 
     @Test
-    fun testGetItems() = runBlocking {
+    fun testGetItemsEmptyList() = runBlocking {
         val restApi = mock<RestApi> {
             on { runBlocking { getListItems() } } doReturn ProductList(listOf())
         }
         val result = restApi.getListItems()
         assert(result.list.isEmpty())
+    }
+
+    @Test
+    fun testGetItemsWithItem() = runBlocking {
+        val restApi = mock<RestApi> {
+            on { runBlocking { getListItems() } } doReturn ProductList(
+                listOf(
+                    Item(
+                        1,
+                        "item",
+                        "99 kg",
+                        9,
+                        "9",
+                        ""
+                    )
+                )
+            )
+        }
+        val result = restApi.getListItems()
+        assert(result.list.isNotEmpty())
     }
 
 }
